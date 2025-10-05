991cb1c10b57ac5757d61937e2f6b1ce71ce1d96
Julian Büttner <julian.buettner@medicalvalues.de>
2025-10-05 16:54:29 +0200

    Fixed obliterate

--- src/luacommands/obliterate.rs
+++ src/luacommands/obliterate.rs
@@ -1,4 +1,7 @@
+use core::error;
+
 use redis::{ErrorKind, RedisError};
+use thiserror::Error;
 
 use crate::{
     luacommands::{InvokeLuaScript, OBLITERATE},
@@ -11,38 +14,55 @@ pub struct Obliterate<'a> {
     pub force: bool,
 }
 
-pub enum ObliterateReturn {
+pub enum ObliterateOk {
+    /// A batch of jobs have been purged
     Progress,
+    /// The last batch has been purged, the queue is obliterated
     Obliterated,
-    /// Use force to overwrite
+}
+
+#[derive(Debug, Error)]
+pub enum ObliterateErr {
+    /// Jobs are active, use force to overwrite anyways
+    #[error("queue can only be obliterated after all jobs are done")]
     ActiveJobs,
+    /// Queue is not paused
+    #[error("queue can only be obliterated if paused")]
     NotPaused,
+    /// Lua script returned unexpected exit code
+    #[error("unexpected lua script return value: {0}")]
+    UnexpectedLuaExitCode(i32),
+    /// Some error occured in the Redis protocol
+    #[error("something went wrong with redis: {0:?}")]
+    RedisError(#[from] redis::RedisError),
 }
 
 impl<'a> InvokeLuaScript for Obliterate<'a> {
-    type Return = ObliterateReturn;
+    type DomainOk = ObliterateOk;
+    type DomainErr = ObliterateErr;
+    type RedisOutput = i32;
 
-    async fn call(
-        self,
-        con: &mut impl redis::aio::ConnectionLike,
-    ) -> redis::RedisResult<Self::Return> {
-        let exit_code: i32 = OBLITERATE
+    fn generate_invocation(&self) -> Result<redis::ScriptInvocation<'static>, Self::DomainErr> {
+        let mut invoc = OBLITERATE.prepare_invoke();
+        invoc
             .key(self.queue.meta())
             .key(self.queue.base())
             .arg(self.batch_size)
-            .arg(if self.force { "force" } else { "" })
-            .invoke_async(con)
-            .await?;
-        match exit_code {
-            0 => Ok(ObliterateReturn::Obliterated),
-            1 => Ok(ObliterateReturn::Progress),
-            -1 => Ok(ObliterateReturn::NotPaused),
-            -2 => Ok(ObliterateReturn::ActiveJobs),
-            x => Err(RedisError::from((
-                ErrorKind::ResponseError,
-                "Unexpected return code from obliterate job",
-                format!("Exit code {x}"),
-            ))),
+            .arg(if self.force { "force" } else { "" });
+        Ok(invoc)
+    }
+
+    fn map_redis_error(&self, error: RedisError) -> Self::DomainErr {
+        error.into()
+    }
+
+    fn map_value(&self, value: Self::RedisOutput) -> Result<Self::DomainOk, Self::DomainErr> {
+        match value {
+            0 => Ok(ObliterateOk::Obliterated),
+            1 => Ok(ObliterateOk::Progress),
+            -1 => Err(ObliterateErr::NotPaused),
+            -2 => Err(ObliterateErr::ActiveJobs),
+            x => Err(ObliterateErr::UnexpectedLuaExitCode(x)),
         }
     }
 }
